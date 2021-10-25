a8292c3890c179d681215fc0030e21666d1e180e
jachva95 <jachvadzedachi@gmail.com>
2021-10-25 19:29:29 +0400

    fixed qr block generation

--- QRMaker/Program.cs
+++ QRMaker/Program.cs
@@ -11,9 +11,7 @@ namespace QRMaker
 {
     class Program
     {
-        static int _cubeHeight = 1;
-        static int _cubeWidth = 1;
-        static int _cubeLength = 1;
+        private static float _qrHeight = 0.5f;
 
         static void Main(string[] args)
         {
@@ -21,7 +19,10 @@ namespace QRMaker
             stlFile.SolidName = "my-solid";
 
 
-            stlFile.Triangles.AddRange(BuildSquareBlock(0));
+            //stlFile.Triangles.AddRange(BuildSquareBlock(0));
+            stlFile.Triangles.AddRange(BuildSquareBlock(1, 0));
+            stlFile.Triangles.AddRange(BuildSquareBlock(2, 0));
+
 
             using (FileStream fs = new FileStream(@"C:\Users\jachv\source\repos\QRMaker\QRMaker\bin\Debug\net5.0\ref\test.stl", FileMode.OpenOrCreate))
             {
@@ -40,42 +41,49 @@ namespace QRMaker
             }
         }
 
-        static IEnumerable<StlTriangle> BuildSquareBlock(int index)
+        static IEnumerable<StlTriangle> BuildSquareBlock(int indexX, int indexY)
         {
 
             List<StlVertex> startingPoints = new List<StlVertex>(){
-                new StlVertex(-1,-1,-1),
-                new StlVertex(1,1,-1),
-                new StlVertex(1,-1,1),
-                new StlVertex(-1,1,1)
+                new StlVertex(indexX, indexY, 0),
+                new StlVertex(indexX + 1,indexY + 1, 0),
+                new StlVertex(indexX + 1,indexY,_qrHeight),
+                new StlVertex(indexX,indexY + 1,_qrHeight)
             };
+
             var response = new List<StlTriangle>();
 
             foreach (var nextPoint in startingPoints)
             {
+                var xDirection = nextPoint.X > indexX ? -1 : 1;
+                var yDirection = nextPoint.Y > indexY ? -1 : 1;
+
                 foreach (var plane in Enum.GetValues(typeof(Plane)))
                 {
-                    response.Add(GetAdjacentTriangles(nextPoint, (Plane)plane));
+                    response.Add(GetAdjacentTriangles(nextPoint, (Plane)plane, xDirection, yDirection));
                 }
             }
 
             return response;
         }
 
-        static StlTriangle GetAdjacentTriangles(StlVertex point, Plane plane)
+        static StlTriangle GetAdjacentTriangles(StlVertex point, Plane plane, int xDirection, int yDirection)
         {
             StlNormal normal;
+            bool isTop = point.Z > 0;
+            var zDirection = isTop ? 0 : _qrHeight;
+
             switch (plane)
             {
                 case Plane.XY:
-                    normal = new StlNormal(0, 0, point.Z);
-                    return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X * -1, point.Y, point.Z), new StlVertex(point.X, point.Y * -1, point.Z));
+                    normal = new StlNormal(0, 0, isTop ? 1 : -1);
+                    return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X + xDirection, point.Y, point.Z), new StlVertex(point.X, point.Y + yDirection, point.Z));
                 case Plane.YZ:
-                    normal = new StlNormal(point.X, 0, 0);
-                    return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X, point.Y * -1, point.Z), new StlVertex(point.X, point.Y, point.Z * -1));
+                    normal = new StlNormal(point.X > 0 ? 1 : -1, 0, 0);
+                    return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X, point.Y + yDirection, point.Z), new StlVertex(point.X, point.Y, zDirection));
                 case Plane.XZ:
-                    normal = new StlNormal(0, point.Y, 0);
-                    return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X * -1, point.Y, point.Z), new StlVertex(point.X, point.Y, point.Z * -1));
+                    normal = new StlNormal(0, point.Y > 0 ? 1 : -1, 0);
+                    return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X + xDirection, point.Y, point.Z), new StlVertex(point.X, point.Y, zDirection));
                 default:
                     return null;
             }
