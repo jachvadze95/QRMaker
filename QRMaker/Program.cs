403d9619749bd2b7dd6969495e22ff203491f02a
jachva95 <jachvadzedachi@gmail.com>
2021-10-25 17:52:18 +0400

    fixed 1 cube generation

--- QRMaker/Program.cs
+++ QRMaker/Program.cs
@@ -20,13 +20,8 @@ namespace QRMaker
             StlFile stlFile = new StlFile();
             stlFile.SolidName = "my-solid";
 
-            
 
-            for(int i=0; i < 1; i++)
-            {
-                stlFile.Triangles.AddRange(BuildSquareBlock(i));
-            }
-
+            stlFile.Triangles.AddRange(BuildSquareBlock(0));
 
             using (FileStream fs = new FileStream(@"C:\Users\jachv\source\repos\QRMaker\QRMaker\bin\Debug\net5.0\ref\test.stl", FileMode.OpenOrCreate))
             {
@@ -42,50 +37,18 @@ namespace QRMaker
                 stlFile = StlFile.Load(fs);
 
                 var groupedByNomal = stlFile.Triangles.GroupBy(x => x.Normal);
-
             }
         }
-
-
-        //static List<StlTriangle> Create3DRectangleOnPlane(Plane plane, Point start)
-        //{
-        //    var stlFile = new StlFile();
-
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(), new StlVertex(start.X, start.Y, 0), new StlVertex(1, 0, 0), new StlVertex(1, 1, 0)));
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(0, 0, -1), new StlVertex(0, 1, 0), new StlVertex(1, 1, 0), new StlVertex(0, 0, 0)));
-
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(0, -1, 0), new StlVertex(1, 0, 0), new StlVertex(0, 0, 0), new StlVertex(0, 0, 1)));
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(0, -1, 0), new StlVertex(1, 0, 0), new StlVertex(1, 0, 1), new StlVertex(0, 0, 1)));
-
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(1, 0, 0), new StlVertex(0, 0, 0), new StlVertex(0, 1, 0), new StlVertex(0, 0, 1)));
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(1, 0, 0), new StlVertex(0, 1, 0), new StlVertex(0, 1, 1), new StlVertex(0, 0, 1)));
-
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(0, 0, 1), new StlVertex(0, 0, 1), new StlVertex(1, 0, 1), new StlVertex(1, 1, 1)));
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(0, 0, 1), new StlVertex(0, 1, 1), new StlVertex(1, 1, 1), new StlVertex(0, 0, 1)));
-
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(0, 1, 0), new StlVertex(1, 1, 0), new StlVertex(0, 1, 0), new StlVertex(0, 1, 1)));
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(0, 1, 0), new StlVertex(1, 1, 0), new StlVertex(1, 1, 1), new StlVertex(0, 1, 1)));
-
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(-1, 0, 0), new StlVertex(1, 0, 0), new StlVertex(1, 1, 0), new StlVertex(1, 0, 1)));
-        //    stlFile.Triangles.Add(new StlTriangle(new StlNormal(-1, 0, 0), new StlVertex(1, 1, 0), new StlVertex(1, 1, 1), new StlVertex(1, 0, 1)));
-        //}
-
-        
-
 
         static IEnumerable<StlTriangle> BuildSquareBlock(int index)
         {
 
-            List<StlVertex> startingPoints = new List<StlVertex>(){ 
-                new StlVertex(0,0,0),
-                new StlVertex(1,0,1),
-                new StlVertex(0,1,1),
-                new StlVertex(1,1,0)
+            List<StlVertex> startingPoints = new List<StlVertex>(){
+                new StlVertex(-1,-1,-1),
+                new StlVertex(1,1,-1),
+                new StlVertex(1,-1,1),
+                new StlVertex(-1,1,1)
             };
-
-            
-            startingPoints.ForEach(x => x.X += index);
-
             var response = new List<StlTriangle>();
 
             foreach (var nextPoint in startingPoints)
@@ -98,21 +61,20 @@ namespace QRMaker
 
             return response;
         }
-         
+
         static StlTriangle GetAdjacentTriangles(StlVertex point, Plane plane)
         {
-            var normal = new StlNormal();
-
+            StlNormal normal;
             switch (plane)
             {
                 case Plane.XY:
-                    normal = new StlNormal(0, 0, -1);
+                    normal = new StlNormal(0, 0, point.Z);
                     return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X * -1, point.Y, point.Z), new StlVertex(point.X, point.Y * -1, point.Z));
                 case Plane.YZ:
-                    normal = new StlNormal(-1, 0, 0);
+                    normal = new StlNormal(point.X, 0, 0);
                     return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X, point.Y * -1, point.Z), new StlVertex(point.X, point.Y, point.Z * -1));
                 case Plane.XZ:
-                    normal = new StlNormal(0, -1, 0);
+                    normal = new StlNormal(0, point.Y, 0);
                     return new StlTriangle(normal, new StlVertex(point.X, point.Y, point.Z), new StlVertex(point.X * -1, point.Y, point.Z), new StlVertex(point.X, point.Y, point.Z * -1));
                 default:
                     return null;
